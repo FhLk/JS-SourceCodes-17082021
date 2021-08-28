2ed520f9f352b001cabf28396f3a9b63fc25d66d
FhLk <paramat10042545@hotmail.com>
2021-08-28 11:22:25 +0700

    Update Study

---
+++ Study.js
@@ -0,0 +1,6 @@
+console.log(1==true) //1==1
+console.log("1"==[]) //"1"=="", 1==0, true==true,
+console.log("1"==1) //"1"--> 1
+console.log(1==null) //
+console.log("1"==undefined)
+console.log(null==0)
